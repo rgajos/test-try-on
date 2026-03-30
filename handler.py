7e54fe1e0280d5b58cdeaa7647ba9a183352c2f2
rgajos <rad1011@wp.pl>
2026-03-30 12:07:26 +0200

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,6 +1,12 @@
 import runpod
 
-def handler(job):
-    return {"message": "działa 🚀"}
+def handler(event):
+    input_data = event["input"]
+
+    # testowy output (na start!)
+    return {
+        "message": "Serverless działa 🚀",
+        "input": input_data
+    }
 
 runpod.serverless.start({"handler": handler})
